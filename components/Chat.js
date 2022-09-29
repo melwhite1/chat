50b33eb076fe39291164314f74523610c4c8b7df
melwhite1 <melissawhite668@gmail.com>
2022-09-29 11:48:29 -0400

    Add firestore

--- components/Chat.js
+++ components/Chat.js
@@ -1,43 +1,111 @@
 import React from 'react';
 import { GiftedChat, Bubble } from 'react-native-gifted-chat';
-import { View, Text, Button, Platform, KeyboardAvoidingView } from 'react-native';
+import { View, Platform, KeyboardAvoidingView } from 'react-native';
+
+const firebase = require('firebase');
+require('firebase/firestore');
 
 export default class Chat extends React.Component {
   constructor() {
     super();
     this.state = {
       messages: [],
-    }
+      uid: 0,
+      user: {
+        _id: '',
+        name: '',
+      },
+    };
+
+  // Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCoqHxPjptB1fnkr2TnYuI6eagOLKmHjRc",
+  authDomain: "chat-app-17c51.firebaseapp.com",
+  projectId: "chat-app-17c51",
+  storageBucket: "chat-app-17c51.appspot.com",
+  messagingSenderId: "915003979239",
+  appId: "1:915003979239:web:88668c9aab6c32d6783942",
+  measurementId: "G-707VW0VG1Z"
+};
+
+if (!firebase.apps.length){
+  firebase.initializeApp(firebaseConfig);
   }
 
-  componentDidMount() {
-   this.setState({
-     messages: [
-       {
-         _id: 1,
-         text: 'Hello developer',
-         createdAt: new Date(),
-         user: {
-           _id: 2,
-           name: 'React Native',
-           avatar: 'https://placeimg.com/140/140/any',
-         },
-        },
-        {
-         _id: 2,
-         text: 'This is a system message',
-         createdAt: new Date(),
-         system: true,
-        },
-      ]
-   })
- }
+  //create reference to 'messages' collection
+  this.referenceMessages = firebase.firestore().collection('messages');
+}
 
- onSend(messages = []) {
-   this.setState(previousState => ({
-     messages: GiftedChat.append(previousState.messages, messages),
-   }))
- }
+onCollectionUpdate = (querySnapshot) => {
+  const messages = [];
+  // go through each document
+  querySnapshot.forEach((doc) => {
+    // get the QueryDocumentSnapshot's data
+    let data = doc.data();
+    messages.push({
+      _id: data._id,
+      text: data.text,
+      createdAt: data.createdAt.toDate(),
+      user: {
+        _id: data.user._id,
+        name: data.user.name,
+      },
+    });
+  });
+  this.setState({
+    messages,
+  });
+}
+
+addMessages() {
+  const message = this.state.messages[0];
+  this.referenceMessages.add({
+    _id: message._id,
+    text: message.text || "",
+    createdAt: message.createdAt,
+    user: message.user,
+  });
+}
+
+componentDidMount() {
+  // Set name as title chat
+  let { name } = this.props.route.params;
+  this.props.navigation.setOptions({ title: name });
+
+  // Reference to load messages from Firebase
+  this.referenceChatMessages = firebase.firestore().collection('messages');
+
+  // Authenticate user anonymously
+  this.authUnsubscribe = firebase.auth().onAuthStateChanged((user) => {
+    if (!user) {
+      firebase.auth().signInAnonymously();
+    }
+    this.setState({
+      uid: user.uid,
+      messages: [],
+      user: {
+        _id: user.uid,
+        name: name,
+      },
+    });
+    this.unsubscribe = this.referenceChatMessages
+      .orderBy('createdAt', 'desc')
+      .onSnapshot(this.onCollectionUpdate);
+  });
+}
+
+componentWillUnmount() {
+  this.unsubscribe();
+  this.authUnsubscribe();
+}
+
+onSend(messages = []) {
+  this.setState((previousState) => ({
+    messages: GiftedChat.append(previousState.messages, messages),
+  }),() => {
+    this.addMessages();
+  });
+}
  renderBubble(props) {
    return (
      <Bubble
@@ -59,9 +127,7 @@ export default class Chat extends React.Component {
           renderBubble={this.renderBubble.bind(this)}
           messages={this.state.messages}
           onSend={(messages) => this.onSend(messages)}
-          user={{
-            _id: 1,
-          }}
+          user={{ _id: this.state.user._id, name: name }}
         />
         {Platform.OS === 'android' ? (
            <KeyboardAvoidingView behavior="height" />
